2e7cfd93802c774266e1cdeb522cd3c5bf3620fa
Santiago Hernandez <C.SAHE@tessa.com.uy>
2025-06-06 21:20:33 +0200

    agregar seeder bd

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+
+INSERT INTO users (name, email, password) VALUES ('Admin', 'admin@example.com', '12345');
